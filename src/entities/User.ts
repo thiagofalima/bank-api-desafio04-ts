9836defdefbf56ecb1884fd8c3c2453830d25339
thiagofalima <thiagofalima@hotmail.com>
2026-04-17 16:38:55 -0300

    refactor: tsconfig adjust

---
+++ src/entities/User.ts
@@ -0,0 +1,31 @@
+import { randomUUID } from "node:crypto";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('user')
+export class User {
+
+    @PrimaryGeneratedColumn()
+    user_id: string
+
+    @Column({ nullable: false })
+    name: string
+
+    @Column({ nullable: false })
+    email: string
+
+    @Column({ nullable: false })
+    password: string
+
+    constructor(
+        user_id: string,
+        name: string,
+        email: string,
+        password: string
+    ){
+        this.user_id = randomUUID()
+        this.name = name
+        this.email = email
+        this.password = password
+    }
+
+}
