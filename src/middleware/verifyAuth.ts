9f80f8c1b18d3f69e06ce4e9c7f1b41e4ca10411
thiagofalima <thiagofalima@hotmail.com>
2026-04-22 16:55:26 -0300

    feat: add auth

---
+++ src/middleware/verifyAuth.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express"
+import { verify } from "jsonwebtoken"
+
+export function verifyAuth(request: Request, response: Response, next: NextFunction) {
+    const authToken = request.headers.authorization
+
+    if(authToken){
+        const [, token] = authToken.split(" ")
+
+        try{
+            const { sub } = verify(token, "123456789")
+            return next()
+        } catch (error){
+            return response.status(401).json({message: "Unauthorized"})
+        }
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,14 @@
 import {Router} from "express";
 import {UserController} from "./controllers/UserController";
 import { LoginController } from "./controllers/LoginController";
+import { verifyAuth } from "./middleware/verifyAuth";
 
 export const router = Router();
 const userController = new UserController();
 const loginController = new LoginController();
 
 router.post("/user", userController.createUser); // Nao precisa de parametro aqui, ele entende por default
-router.get("/user", userController.getUser);
+router.get("/user/:userId", verifyAuth,userController.getUser); // para "decorar" como se fosse
 router.delete('/user', userController.deleteUser);
 
 router.post("/login", loginController.login)
