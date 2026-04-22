cc715e8bfe5a2ae97a4d4e0f2eda7598b2cd37f9
thiagofalima <thiagofalima@hotmail.com>
2026-04-22 15:35:52 -0300

    feat: add getUserByEmailAndId in UserRepository

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,15 +1,28 @@
-import { Request, Response } from "express"
-import { sign } from "jsonwebtoken"
+import { Request, Response } from "express";
+import { sign } from "jsonwebtoken";
 
 const user = {
-    user_id: "12345",
-    name: "Thiago",
-    email: "thiago@senai.com",
-    password: "12345"
-}
+  user_id: "12345",
+  name: "Thiago",
+  email: "thiago@senai.com",
+  password: "12345",
+};
 
-export class LoginController{
-    login =  async (request: Request, response: Response) => {
-        return response.status(200).json(user)
-    }
+export class LoginController {
+  login = async (request: Request, response: Response) => {
+    const tokenData = {
+      name: user.name,
+      email: user.email,
+    };
+
+    const tokenKey = "123456789";
+
+    const tokenOptions = {
+      subject: user.user_id,
+    };
+
+    const token = sign(tokenData, tokenKey, tokenOptions);
+
+    return response.status(200).json({ token });
+  };
 }
--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -21,4 +21,13 @@ export class UserRepository {
             }
         })
     }
+
+    getUserByEmailAndPassword = async (email: string, password: string): Promise<User | null> => {
+        return this.manager.findOne(User, {
+            where: {
+                email,
+                password
+            }
+        })
+    }
 }
