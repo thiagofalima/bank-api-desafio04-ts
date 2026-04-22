865c1ddc584467e9d09fb576df5b4d6876c65494
thiagofalima <thiagofalima@hotmail.com>
2026-04-22 13:35:04 -0300

    feat: UserRepository test and mocks

---
+++ src/repositories/UserRepository.spec.ts
@@ -0,0 +1,25 @@
+import { getMockEntityManager } from "../__mocks__/mockEntityMAnager.mock"
+import { UserRepository } from "UserRepository"
+import { User } from "../entities/User"
+import { EntityManager } from "typeorm"
+
+test("Deve cadastrar um novo usuário no banco de dados", () => {
+    let userRepository: new UserRepository
+    let managerMock: Partial<EntityManager>
+
+    const mockUSer: User = {
+        id_user: "12345",
+        name: "test",
+        email: "test@test.com",
+        password: "12345"
+
+    }
+    beforeAll(async () => {
+        managerMock = await getMockEntityManager({})
+        userRepository = new userRepository(managerMock as EntityManager)
+    })
+
+    await userRepository.createUSer(mockUser)
+    expect(managerMock.save())
+
+})
